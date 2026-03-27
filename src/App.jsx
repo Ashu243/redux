c97cc8644bf7db4021054736f056468fb5093742
Ashu <ashuv6309@gmail.com>
2026-03-27 23:28:06 +0530

    add increment / decrement functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/features/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const count = useSelector((state)=> state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <>
     <h2>hey, redux learning is happening!!</h2>
+    <button onClick={()=> dispatch(increment())} >increment</button>
+    <button onClick={()=> dispatch(decrement())} >decrement</button>
+    <div>count: {count}</div>
     </>
   )
 }
